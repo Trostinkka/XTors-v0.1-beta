57211b9bf1a91f36ca1f1fb145958a55e8bdd445
Trostinkka <oaleksand5@gmail.com>
2024-04-24 03:50:52 +0300

    Добавьте файлы проекта.

---
+++ XTors/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XTors
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ XTors/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace XTors
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
